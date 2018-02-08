6c51498e365b39c5299e78d2f1d7cc9509f45aee
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2018-02-08 23:26:35 +0300

    #5 Build sections from the ini file into the Commands slice

--- config/commands.go
+++ config/commands.go
@@ -3,6 +3,7 @@ package config
 import (
 	"os"
 	"gopkg.in/ini.v1"
+	"strings"
 )
 
 var configDir = os.ExpandEnv("$HOME/.vmx")
@@ -13,13 +14,19 @@ type Command struct {
 
 func ReadCommands() []Command {
 	cfg, err := ini.Load(configDir + "/commands")
+	cfg.BlockMode = false
 	if err != nil {
 		os.Exit(1)
 	}
 	sections := cfg.Sections()
-	commands := make([]Command, len(sections))
+	// There is always DEFAULT section, so exclude that one from the commands capacity
+	commands := make([]Command, 0, len(sections)-1)
 	for _, section := range sections {
-		commands = append(commands, Command{section.Name(), section.Body()})
+		name := section.Name()
+		if strings.Compare(name, "DEFAULT") == 0 {
+			continue
+		}
+		commands = append(commands, Command{section.Name(), section.Key("command").String()})
 	}
 	return commands
 }
