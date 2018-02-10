a7b9a2c0b6355d86435118de74bd99c3276fba67
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2018-02-10 23:41:16 +0300

    #3 Add initial support for reading hosts config file

--- command/run.go
+++ command/run.go
@@ -27,15 +27,36 @@ func readCommands(config config.Config) map[string]Command {
 		if strings.Compare(name, "DEFAULT") == 0 {
 			continue
 		}
-		commands[section.Name()] = Command{section.Name(), section.Key("command").String()}
+		commands[name] = Command{name, section.Key("command").String()}
 	}
 	return commands
+}
+
+func readHosts(config config.Config) map[string][]string {
+	hosts := make(map[string][]string)
+	cfg, err := ini.LoadSources(ini.LoadOptions{AllowBooleanKeys: true}, config.Dir + "/hosts")
+	cfg.BlockMode = false
+	if err != nil {
+		os.Exit(1)
+	}
+	sections := cfg.Sections()
+	// There is always DEFAULT section, so exclude that one from the commands capacity
+	for _, section := range sections {
+		name := section.Name()
+		if strings.Compare(name, "DEFAULT") == 0 {
+			continue
+		}
+		hosts[name] = section.KeyStrings()
+	}
+	return hosts
 }
 
 // CmdRun runs custom command
 func CmdRun(c *cli.Context) {
 	commands := readCommands(config.DefaultConfig)
-	commandName := c.Args().First()
+	args := c.Args()
+	host := args.Get(0)
+	commandName := args.Get(1)
 	command, ok := commands[commandName]
 	if !ok {
 		names := make([]string, len(commands))
@@ -48,5 +69,7 @@ func CmdRun(c *cli.Context) {
 		fmt.Fprintf(os.Stdout, "Known list of custom commands are: %s\n", strings.Join(names, ", "))
 		os.Exit(1)
 	}
-	fmt.Fprintf(os.Stdout, "Running command: %s\n", command.command)
+	hosts := readHosts(config.DefaultConfig)
+	target := hosts[host]
+	fmt.Fprintf(os.Stdout, "Running command: %s on %v\n", command.command, target)
 }
