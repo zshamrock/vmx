58ee58a246b94435622ccee976059b24ae84900d
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2018-05-05 23:47:30 +0300

    #58 Prepare test data before start working on the feature

--- command/command_test.go
+++ command/command_test.go
@@ -29,6 +29,28 @@ func TestGetCommand(t *testing.T) {
 	}
 }
 
+func TestGetCommandExtraArgs(t *testing.T) {
+	followFlags := []string{"", "-f", "--follow"}
+	for _, followFlag := range followFlags {
+		flags := flag.FlagSet{}
+		flags.Bool("follow", false, "")
+		commandText := "tail -f -n 10 logs/rest.log"
+		flags.Parse([]string{"--", followFlag, "dev", commandText})
+		app := cli.NewApp()
+		context := cli.NewContext(app, &flags, nil)
+		command, extraArgs := getCommand(context, true)
+		if !command.IsAdHoc() {
+			t.Errorf("Command name should be ad-hoc, but got %s", command.Name)
+		}
+		if command.Command != commandText {
+			t.Errorf("Command should be %s, but got %s", commandText, command.Command)
+		}
+		if extraArgs != "" {
+			t.Errorf("Extra args should be empty, but got %s", extraArgs)
+		}
+	}
+}
+
 func TestContainsFollow(t *testing.T) {
 	followFlags := []string{"-f", "--follow"}
 	for _, followFlag := range followFlags {
---
+++ test/README.md
@@ -0,0 +1,6 @@
+There are 2 env variables which control where the `vmx` looks for the application config and SSH config.
+
+They are: `VMX_HOME` and `VMX_SSH_CONFIG_HOME`.
+
+So the data in this directory is used for testing when the corresponding env variables set to point to `config` and
+`ssh` inner directories correspondingly.
